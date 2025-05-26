243f37771ef55d406ef02e0e19320ba2152646c3
Sidharth <78693144+sidzx@users.noreply.github.com>
2025-05-26 12:46:43 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -198,13 +198,7 @@ function Home() {
                         }} component="div">
                           {item.name}
                         </Typography>
-                        <Typography variant="body2" style={{
-
-                          alignSelf: "center"
-                        }} sx={{ color: 'text.secondary' }}>
-                          {item.description}
-
-                        </Typography>
+                       
                         <Typography variant="body2" style={{
                           fontWeight: "bold",
                           color: "green",
